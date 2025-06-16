9152dc9bd9ae415aadae55d106e94330b3629fdc
Larry <melarry46@gmail.com>
2025-06-16 09:18:35 +0300

    done

--- server/controllers/restaurant_controller.py
+++ server/controllers/restaurant_controller.py
@@ -5,7 +5,7 @@ from ..config import db
 
 restaurant_bp = Blueprint("restaurants", __name__, url_prefix="/restaurants")
 
-@restaurant_bp.route("", methods=["GET"])
+@restaurant_bp.route("/", methods=["GET"])
 def get_restaurants():
     restaurants = Restaurant.query.all()
     return jsonify([{"id": r.id, "name": r.name, "address": r.address} for r in restaurants])
